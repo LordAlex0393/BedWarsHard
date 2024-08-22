f6e0e787a8b24077807e2dad92e3d482b813b0df
Lord_WorkPC <alexstone0390@gmail.com>
2024-08-22 14:02:55 +0700

    v0.2.24 - starting-delay completer update

--- src/main/java/org/lordalex/bedwarshard/Commands/GameTabCompleter.java
+++ src/main/java/org/lordalex/bedwarshard/Commands/GameTabCompleter.java
@@ -22,6 +22,7 @@ public class GameTabCompleter implements TabCompleter {
             list.add("edit-shop");
             list.add("limit-players");
             list.add("game-length");
+            list.add("starting-delay");
             list.add("bed-drop");
         } else if(args.length < 2){
             list.add("start");
